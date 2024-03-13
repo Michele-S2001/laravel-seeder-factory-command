1a6f8008508d7c393c2781813c7d5adfae6391ef
MicheleSerafini <serafini.michele01@gmail.com>
2024-03-13 19:14:52 +0100

    FEAT > Categories & Products seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\DBAL\TimestampType;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $csvCategoriesFile = database_path('csv/categories.csv');
+    $csvCategoriesData = array_map('str_getcsv', file($csvCategoriesFile));
+
+    $currTimeStamp = Carbon::now();
+
+    foreach($csvCategoriesData as $row) {
+      DB::table('categories')->insert([
+        'name' => $row[0],
+        'hex' => $row[1],
+        'created_at' => $currTimeStamp,
+        'updated_at' => $currTimeStamp
+      ]);
     }
+  }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,16 +2,36 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
+  /**
+   * Run the database seeds.
+   */
+  public function run(Faker $faker): void
+  {
+
+    $categories_ids = Category::all()->pluck('id');
+
+    for($i = 0; $i < 100; $i++) {
+      $isFeatured = $i < 5 ? 1 : 0;
+
+      $new_product = new Product([
+        'category_id' => $categories_ids->random(),
+        'name' => $faker->words(rand(2, 3), true),
+        'description' => $faker->paragraph(3),
+        'url' => "https://picsum.photos/id/" . $i + 1 . "/200/300",
+        'ean' => $faker->ean13(),
+        'price' => $faker->randomFloat(2, 20, 99),
+        'is_featured' => $isFeatured
+      ]);
+
+      $new_product->save();
     }
+  }
 }
